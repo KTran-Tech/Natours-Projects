565bc2f0f4255dfebd242b05d3d730576fcf628d
KTran-Tech <kevinmt191919@gmail.com>
2020-05-21 15:02:35 -0500

    Mongoose

--- server.js
+++ server.js
@@ -1,8 +1,47 @@
+const mongoose = require('mongoose');
+
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
+
+//  #) MongoDB
+const DB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD
+);
+//use to get rid of some deprecation warnings
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    console.log('DB connection successful')
+  );
+
+//schema(outline/model)
+//specify a schema for our data
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
 
 // #) START SERVER
 const port = process.env.PORT || 3000;
