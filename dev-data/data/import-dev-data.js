b208cb94b18b4415bef8f54ccaec0a3363325a80
KTran-Tech <kevinmt191919@gmail.com>
2020-05-21 23:57:57 -0500

    default file

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,65 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
+
+const Tour = require('../../models/tourModel');
+//
+
+//  #) MongoDB
+const DB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD
+);
+//use to get rid of some deprecation warnings
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    console.log('DB connection successful')
+  );
+
+//
+
+// To READ JSON FILE
+
+const tours = JSON.parse(
+  fs.readFileSync(
+    `${__dirname}/tours-simple.json`,
+    'utf-8'
+  )
+);
+
+// To IMPORT DATA INTO DATABASE
+
+//creates an array of javascript object
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data was successfully loaded');
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data successfully deleted!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
