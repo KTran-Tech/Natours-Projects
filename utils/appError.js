0e953789daaa78ecd98323bbf4038f689bf2e089
KTran-Tech <kevinmt191919@gmail.com>
2020-05-26 14:18:31 -0500

    Refactoring

---
+++ utils/appError.js
@@ -0,0 +1,20 @@
+class AppError extends Error {
+  constructor(message, statusCode) {
+    //Erorr is a built in class, so its calling a 'message' method that returns an error msg
+    //gets back error message
+    super(message);
+
+    this.statusCode = statusCode;
+    this.status = `${statusCode}`.startsWith('4')
+      ? 'fail'
+      : 'error';
+    this.isOperational = true;
+
+    Error.captureStackTrace(
+      this,
+      this.constructor
+    );
+  }
+}
+
+module.exports = AppError;
