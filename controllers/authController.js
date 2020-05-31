98c2192933da3d807a0c1407b6f0649f460396f2
KTran-Tech <kevinmt191919@gmail.com>
2020-05-30 23:54:19 -0500

    Quick Change

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 exports.signup = catchAsync(
   async (req, res, next) => {
@@ -29,3 +30,24 @@ exports.signup = catchAsync(
     });
   }
 );
+
+exports.login = (req, res, next) => {
+  const { email, password } = req.body;
+
+  //1) Check if email and password exist
+  if (!email || !password) {
+    //call upon next middleware
+    next(
+      new AppError(
+        'Please provide email and password!',
+        400
+      )
+    );
+  }
+  //3) If everything works, send token to client
+  const token = '';
+  res.status(200).json({
+    status: 'success',
+    token,
+  });
+};
