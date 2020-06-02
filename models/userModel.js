6f075ebf5a5a5dfad63b71d1c58991457e638b5f
KTran-Tech <kevinmt191919@gmail.com>
2020-06-01 22:54:08 -0500

    Protecting Tour Routes Part 2 Finished

--- models/userModel.js
+++ models/userModel.js
@@ -13,10 +13,7 @@ const userSchema = new mongoose.Schema({
     required: [true, 'A  must have a '],
     unique: true,
     lowercase: true,
-    validate: [
-      validator.isEmail,
-      'Please provide a valid email',
-    ],
+    validate: [validator.isEmail, 'Please provide a valid email'],
   },
   photo: String,
   password: {
@@ -27,10 +24,7 @@ const userSchema = new mongoose.Schema({
   },
   passwordConfirm: {
     type: String,
-    required: [
-      true,
-      'A  must have a password Confirmation',
-    ],
+    required: [true, 'A  must have a password Confirmation'],
     validate: {
       //Will only work on Create() and Save()
       //pass the property for this object and check if...
@@ -49,10 +43,7 @@ userSchema.pre('save', async function (next) {
   //if password has not been modified then call next middleware
   if (!this.isModified('password')) return next();
   //encrypt password with cost of 12
-  this.password = await bcrypt.hash(
-    this.password,
-    12
-  );
+  this.password = await bcrypt.hash(this.password, 12);
   //delete password field
   //this works because the password is only a required INPUT not required data to be pushed to database
   this.passwordConfirm = undefined;
@@ -64,19 +55,22 @@ userSchema.methods.correctPassword = async function (
   candidatePassword,
   userPassword
 ) {
-  //returns true or false
-  return await bcrypt.compare(
-    candidatePassword,
-    userPassword
-  );
+  //returns true or false by comparing users password vs users database password
+  return await bcrypt.compare(candidatePassword, userPassword);
 };
 
-userSchema.methods.changedPasswordAfter = function (
-  JWTTimestamp
-) {
+userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
   if (this.passwordChangedAt) {
-    console(this.passwordChangedAt, JWTTimestamp);
+    //turns the date into readable iat, this is the initial data sent to the database
+    const changedTimestamp = parseInt(
+      this.passwordChangedAt.getTime() / 1000,
+      10
+    );
+    //
+    // console.log(changedTimestamp, JWTTimestamp);
+    return JWTTimestamp < changedTimestamp;
   }
+  //false means password not changed
   return false;
 };
 
