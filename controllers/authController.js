68010c794a2220685743c3c948db071a5ca87d1b
KTran-Tech <kevinmt191919@gmail.com>
2020-06-04 22:06:27 -0500

    Update Password Setup

--- controllers/authController.js
+++ controllers/authController.js
@@ -305,3 +305,16 @@ exports.resetPassword = catchAsync(
     });
   }
 );
+
+//
+
+//
+
+//
+
+exports.updatePassword = (req, res, next) => {
+  // 1) Get user from collection
+  // 2) Check if POSTed current password is correct
+  // 3) If so, update password
+  // 4) Log user in, send JWT
+};
