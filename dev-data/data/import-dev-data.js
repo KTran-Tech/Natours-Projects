29bab20510f96c9f7311dc0e2f89cee9fc75e7fc
KTran-Tech <kevinmt191919@gmail.com>
2020-06-09 14:24:13 -0500

    Importing Review and User Data

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -6,7 +6,14 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
 const Tour = require('../../models/tourModel');
+const Review = require('../../models/reviewModel');
+const User = require('../../models/userModel');
+
 //
+
+//
+
+//BASIC SETUP
 
 //  #) MongoDB
 const DB = process.env.DATABASE.replace(
@@ -23,20 +30,44 @@ mongoose
   })
   .then(() => console.log('DB connection successful'));
 
+//BASIC SETUP
+
+//
+
+//
+
 //
 
 // To READ JSON FILE
+
 //Also allows database to copy the data from file and use it as its own
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/tours.json`, 'utf-8')
 );
+const users = JSON.parse(
+  fs.readFileSync(`${__dirname}/users.json`, 'utf-8')
+);
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8')
+);
+
+// To READ JSON FILE
+
+//
 
 // To IMPORT DATA INTO DATABASE
 
 //creates an array of javascript object
 const importData = async () => {
   try {
+    //  validateBeforeSave: false, prevents validation errors so you could do changes to your data
+    //
     await Tour.create(tours);
+    await User.create(users, {
+      validateBeforeSave: false,
+    });
+    await Review.create(reviews);
+    //
     console.log('Data was successfully loaded');
   } catch (err) {
     console.log(err);
@@ -45,13 +76,26 @@ const importData = async () => {
 
 const deleteData = async () => {
   try {
+    //Delete All the data in database
+    //
     await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
+    //
     console.log('Data successfully deleted!');
   } catch (err) {
     console.log(err);
   }
+  //
   process.exit();
+  //
 };
+
+// To IMPORT DATA INTO DATABASE
+
+//
+
+//
 
 //To manipulate Database
 //First you have to '--delete' your current data in database and then '--import' in your newer data
@@ -62,3 +106,5 @@ if (process.argv[2] === '--import') {
 } else if (process.argv[2] === '--delete') {
   deleteData();
 }
+
+//
